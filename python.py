5a8f088d24154925e4c38b7963d8f45dfb466b9a
Martinn <martinhruska770@gmail.comgit config --global user.email martinhruska770@gmail.com>
2023-10-02 11:21:38 +0200

    c

---
+++ python.py
@@ -0,0 +1,50 @@
+zamestnanci = {
+    1: {
+        "jmeno": "Martin",
+        "prijmeni": "Hruska",
+        "pozice": "Programátor",
+        "email": "martinhruska@seznam.cz",
+        "kancelar": "C1"
+    },
+    2: {
+        "jmeno": "Pepa",
+        "prijmeni": "Pepina",
+        "pozice": "Kuchař",
+        "email": "Pepik1@seznam.cz",
+        "kancelar": "B7"
+    },
+    3: {
+        "jmeno": "Eliška",
+        "prijmeni": "Pokorná",
+        "pozice": "Učetní",
+        "email": "Epokorna@seznam.cz",
+        "kancelar": "V2"
+    },
+    4: {
+        "jmeno": "Marek",
+        "prijmeni": "Doležal",
+        "pozice": "Asistent",
+        "email": "MarekD@seznam.cz",
+        "kancelar": "G1"
+    }
+}
+
+print("Zde je seznam zaměstnanců:")
+for id_zamestnance, info in zamestnanci.items():
+    print(f"Zaměstnanec {id_zamestnance}:")
+    print(f"Jméno: {info['jmeno']}")
+    print(f"Příjmení: {info['prijmeni']}")
+    print(f"Pozice: {info['pozice']}")
+    print(f"Email: {info['email']}")
+    print(f"Kancelář: {info['kancelar']}")
+    print()
+
+print("Všechny důležité údaje:")
+for id_zamestnance, info in zamestnanci.items():
+    print(f"{info['jmeno']} {info['prijmeni']}, {info['email']}")
+
+
+id_vybraneho_zamestnance = 1  
+vybrany_zamestnanec = zamestnanci.get(id_vybraneho_zamestnance)
+if vybrany_zamestnanec:
+    print(f"Vybraný zaměstnanec: {vybrany_zamestnanec['jmeno']} {vybrany_zamestnanec['prijmeni']}, pozice: {vybrany_zamestnanec['pozice']}")
